2d8ae89c672950c6e20093087293f961d2cf9484
Gabriel Martorelli <gabriel.martorelli@hotmail.com>
2026-08-14 22:11:56 -0300

    refactor: atualiza mapeamento do contato no dbcontext

--- ProverContatos.Infrastructure/Data/ProverContatosDbContext.cs
+++ ProverContatos.Infrastructure/Data/ProverContatosDbContext.cs
@@ -3,21 +3,34 @@ using ProverContatos.Domain.Entities;
 
 namespace ProverContatos.Infrastructure.Data;
 
-public class ProverContatosDbContext(DbContextOptions<ProverContatosDbContext> options) : DbContext(options)
+public class ProverContatosDbContext(
+    DbContextOptions<ProverContatosDbContext> options) : DbContext(options)
 {
     public DbSet<Contato> Contatos { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)//LINQ to Entities garente que o EF converta corretamente as queries em C# para SQL, evitando erros de tradução e garantindo que as consultas sejam executadas corretamente no banco de dados.
+    protected override void OnModelCreating(
+        ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Contato>(entity =>
         {
             entity.ToTable("Contatos");
-            entity.HasKey(c => c.Id);
-            entity.Property(c => c.Nome).IsRequired().HasMaxLength(100);
-            entity.Property(c => c.DataNascimento).IsRequired();
-            entity.Property(c => c.Sexo).IsRequired();
-            entity.Property(c => c.Ativo).IsRequired();
-            entity.Ignore(c => c.Idade);
+
+            entity.HasKey(contato => contato.Id);
+
+            entity.Property(contato => contato.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(contato => contato.DataNascimento)
+                .IsRequired();
+
+            entity.Property(contato => contato.Sexo)
+                .IsRequired();
+
+            entity.Property(contato => contato.Ativo)
+                .IsRequired();
+
+            entity.Ignore(contato => contato.Idade);
         });
 
         base.OnModelCreating(modelBuilder);
