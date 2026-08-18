73093a72c7f07c2bbfbf657778bfb72b4f5e5965
Gabriel Martorelli <gabriel.martorelli@hotmail.com>
2026-08-18 16:09:41 -0300

    refactor(exceptions): simplifica exceção base

--- src/ProverContatos.Exception/ExceptionsBase/ProverException.cs
+++ src/ProverContatos.Exception/ExceptionsBase/ProverException.cs
@@ -2,22 +2,9 @@
 
 namespace ProverContatos.Exception.ExceptionsBase;
 
-public class ProverException : SystemException
+public class ProverException(string message, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+    : System.Exception(message)
 {
-    public IList<string> Errors { get; }// Lista de mensagens de erro associadas à exceção.
-    public HttpStatusCode StatusCode { get; }// Código de status HTTP associado à exceção.
-
-    public ProverException(string message, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
-        : base(message)
-    {
-        Errors = [message];
-        StatusCode = statusCode;
-    }// Construtor que aceita uma única mensagem de erro e um código de status HTTP opcional (padrão é BadRequest).
-
-    public ProverException(IList<string> errors, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
-        : base(string.Join(", ", errors))
-    {
-        Errors = errors;
-        StatusCode = statusCode;
-    }
+    public IList<string> Errors { get; } = [message];
+    public HttpStatusCode StatusCode { get; } = statusCode;
 }
